87106c25bbdc96543b778971da12ea8da51d8818
Ian <iancabello16@gmail.com>
2022-10-20 17:11:21 -0400

    updated how retrieved data is navigated through

--- features/items.ts
+++ features/items.ts
@@ -6,26 +6,26 @@ import { constructErrorMessage } from '../modules/messages/errorMessageGeneratio
 import { normalizeItemName, normalizeStatName,  } from '../modules/cleanup';
 import { itemMap } from '../modules/init';
 
-//Gets item ID from name
+// Gets item ID from name
 export function getItemID(itemName: string): any {
     const normalizedItemName: string = normalizeItemName(itemName);
     return itemMap.get(normalizedItemName);
 } 
 
-//Item API call
+// Item API call
 export async function makeItemAPICall(itemName: string): Promise<any> {
     const itemID = getItemID(itemName);
     return await axios.get(`${basePath}/${region}/items/${itemID}.json`)
-      .then((resp) => {
-        return resp.data;
+      .then((res) => {
+        return res.data;
       });
   }
 
-//Gets item and sends message to Discord
-export async function getItem(itemName: string): Promise<any> {
+// Gets item and sends message to Discord
+export async function fetchItem(itemName: string): Promise<any> {
     return await makeItemAPICall(itemName)
-        .then((data) => {
-            return constructItemEmbedMessage(data);
+        .then((res) => {
+            return constructItemEmbedMessage(res);
         })
         .catch((err) => {
             return constructErrorMessage('Having trouble getting that item\'s info!', 'Are you sure you spelled their name correctly? If so, the API might be down. Try again in a little while.');
@@ -33,34 +33,46 @@ export async function getItem(itemName: string): Promise<any> {
 }
 
 export function constructItemEmbedMessage(itemData: any) {
-    const { name, id, tier, icon, simpleDescription, shop } = itemData;
+    const { name, id, tier, icon, simpleDescription, passives, stats, shop} = itemData;
     const itemMessage = new MessageEmbed()
         .setColor('#87E4E9')
         .setAuthor('The elder drake speaks...', 'https://cdn.discordapp.com/attachments/651148711115882556/877567670608871494/elder-icon.png')
         .setTitle(name)
         .setThumbnail(icon)
-        .setDescription(`${simpleDescription} \nItem ID: ${id}`)  
-        for (var passive in itemData.passives) {
-            if (itemData.passives[passive]) {
-                if (itemData.passives[passive].name !== 'Mythic') {
-                    itemMessage.addFields({ name: itemData.passives[passive].name, value: itemData.passives[passive].effects})
-                }
+
+        if (simpleDescription) {
+            itemMessage.setDescription(`${simpleDescription} \nItem ID: ${id}`);
+        } else {
+            itemMessage.setDescription(`Item ID: ${id}`);
+        };
+
+        Object.entries(passives).forEach((passive: any): void => {
+            const [passiveName, passiveValues] = passive;
+            if (passiveValues.mythic) {
+                itemMessage.addFields({ name: 'Mythic', value: 'No data. To be implemented at a later date', inline: false })
+                itemMessage.setTitle(`${name} (Mythic)`)
+            } else if (passiveValues.unique) {
+                itemMessage.addFields({ name: 'UNIQUE Passive', value: passiveValues.effects, inline: false })
+            } else {
+                itemMessage.addFields({ name: 'Passive', value: passiveValues.effects, inline: false })
             }
-        }
-        for (var stat in itemData.stats) {
-            if (itemData.stats[stat].flat > 0) {
-                itemMessage.addFields({ name: normalizeStatName(stat), value: itemData.stats[stat].flat, inline: false})
+        })
+
+        Object.entries(stats).forEach((itemStat: any): void => {
+            const [statName, statValue] = itemStat;
+            if (statValue.flat > 0) {
+                itemMessage.addFields({ name: normalizeStatName(statName), value: statValue.flat, inline: false });
             }
-        }
-        for (var price in itemData.shop.prices) {
-            if (itemData.shop.prices[price] > 0) {
-                if(price === 'total') {
-                    itemMessage.addFields({ name: 'Buy', value: itemData.shop.prices[price], inline: true})
-                } else {
-                    itemMessage.addFields({ name: capitalizeWordsInString(price), value: itemData.shop.prices[price], inline: true})    
-                }
+        });
+
+        Object.entries(shop.prices).forEach((price: any): void => {
+            const [shopType, shopValue] = price;
+            if (shopType === 'total') {
+                itemMessage.addFields({ name: 'Buy', value: shopValue, inline: true})
+            } else if (shopValue > 0) {
+                itemMessage.addFields({ name: capitalizeWordsInString(shopType), value: shopValue, inline: true})    
             }
-        }
-    console.log(itemMessage);
+        });
+
     return itemMessage;
 }
